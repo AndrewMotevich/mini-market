57571f966269b8b027bf763b7e8b4e4ac96dad2c
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-07-07 14:35:47 +0300

    feat: add style for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFoundPage = () => {
-  return <div>404</div>;
+  return (
+    <div className="not-found-page-container">
+      <h2>404</h2>
+      <p>Page is not found</p>
+    </div>
+  );
 };
 
 export default NotFoundPage;
