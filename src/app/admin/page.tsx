33ff8a763d703860fc45637b625e6f97e6ab0d18
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-07-08 23:29:11 +0300

    fix: te

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -3,11 +3,16 @@ import { SessionProvider } from "next-auth/react";
 import React from "react";
 import LoginComponent from "../../components/adminPanel/login/Login";
 import { Session } from "next-auth";
+import type { AppProps } from "next/app";
 
-const AdminAuthPage = ({ session }: { session: Session }) => {
+const AdminAuthPage = ({
+  pageProps,
+}: AppProps<{
+  session: Session;
+}>) => {
   return (
     <>
-      <SessionProvider session={session}>
+      <SessionProvider session={pageProps.session}>
         <LoginComponent />
       </SessionProvider>
     </>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import ProductTable from "@/components/productTable/ProductTable";
 
 export const revalidate = 60;
 
-export default async function Page() {
+export default async function Home() {
   return (
     <>
       <ProductTable />
