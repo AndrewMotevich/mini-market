0f70b0cdb03ec109b790388a09646435a4c9ec03
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-07-09 11:37:29 +0300

    fix: uncomment all api routes

--- src/app/api/images/route.ts
+++ src/app/api/images/route.ts
@@ -1,24 +1,24 @@
-// import { sql } from "@vercel/postgres";
-// import { NextRequest, NextResponse } from "next/server";
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET() {
-//   try {
-//     return NextResponse.json({
-//       result: await sql`SELECT image_data FROM images WHERE id = 1;`,
-//     });
-//   } catch (error) {
-//     return new Response((error as Error).message, { status: 500 });
-//   }
-// }
+export async function GET() {
+  try {
+    return NextResponse.json({
+      result: await sql`SELECT image_data FROM images WHERE id = 1;`,
+    });
+  } catch (error) {
+    return new Response((error as Error).message, { status: 500 });
+  }
+}
 
-// export async function POST(req: NextRequest) {
-//   try {
-//     const image = await req.json();
-//     return NextResponse.json({
-//       result:
-//         await sql`INSERT INTO images (image_data) VALUES (${image.image_data}) RETURNING id;`,
-//     });
-//   } catch (error) {
-//     return new Response((error as Error).message, { status: 500 });
-//   }
-// }
+export async function POST(req: NextRequest) {
+  try {
+    const image = await req.json();
+    return NextResponse.json({
+      result:
+        await sql`INSERT INTO images (image_data) VALUES (${image.image_data}) RETURNING id;`,
+    });
+  } catch (error) {
+    return new Response((error as Error).message, { status: 500 });
+  }
+}
