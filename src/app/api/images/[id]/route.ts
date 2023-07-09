12f53131fcc06efa21332e505fb4fab50c219417
Andrew <101500007+AndrewMotevich@users.noreply.github.com>
2023-07-09 11:28:12 +0300

    fix: comment all api routes

--- src/app/api/images/[id]/route.ts
+++ src/app/api/images/[id]/route.ts
@@ -1,35 +1,35 @@
-import { sql } from "@vercel/postgres";
-import { NextRequest, NextResponse } from "next/server";
-type Params = { params: { id: string } };
+// import { sql } from "@vercel/postgres";
+// import { NextRequest, NextResponse } from "next/server";
+// type Params = { params: { id: string } };
 
-export async function GET(req: NextRequest, { params }: Params) {
-  try {
-    return NextResponse.json({
-      result: await sql`SELECT image_data FROM images WHERE id = ${params.id}`,
-    });
-  } catch (error) {
-    return new Response((error as Error).message, { status: 500 });
-  }
-}
+// export async function GET(req: NextRequest, { params }: Params) {
+//   try {
+//     return NextResponse.json({
+//       result: await sql`SELECT image_data FROM images WHERE id = ${params.id}`,
+//     });
+//   } catch (error) {
+//     return new Response((error as Error).message, { status: 500 });
+//   }
+// }
 
-export async function PUT(req: NextRequest, { params }: Params) {
-  try {
-    const image = await req.json();
-    return NextResponse.json({
-      result:
-        await sql`UPDATE images SET image_data = ${image.image_data} WHERE id = ${params.id}`,
-    });
-  } catch (error) {
-    return new Response((error as Error).message, { status: 500 });
-  }
-}
+// export async function PUT(req: NextRequest, { params }: Params) {
+//   try {
+//     const image = await req.json();
+//     return NextResponse.json({
+//       result:
+//         await sql`UPDATE images SET image_data = ${image.image_data} WHERE id = ${params.id}`,
+//     });
+//   } catch (error) {
+//     return new Response((error as Error).message, { status: 500 });
+//   }
+// }
 
-export async function DELETE(req: NextRequest, { params }: Params) {
-  try {
-    return NextResponse.json({
-      result: await sql`DELETE FROM images WHERE id = ${params.id}`,
-    });
-  } catch (error) {
-    return new Response((error as Error).message, { status: 500 });
-  }
-}
+// export async function DELETE(req: NextRequest, { params }: Params) {
+//   try {
+//     return NextResponse.json({
+//       result: await sql`DELETE FROM images WHERE id = ${params.id}`,
+//     });
+//   } catch (error) {
+//     return new Response((error as Error).message, { status: 500 });
+//   }
+// }
--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,38 +1,38 @@
-import { kv } from "@vercel/kv";
-import { NextRequest, NextResponse } from "next/server";
-type Params = { params: { id: string } };
+// import { kv } from "@vercel/kv";
+// import { NextRequest, NextResponse } from "next/server";
+// type Params = { params: { id: string } };
 
-export async function GET(req: NextRequest, { params }: Params) {
-  try {
-    return NextResponse.json({
-      result: await kv.json.get("products", `$.${params.id}`),
-    });
-  } catch (error) {
-    return new Response((error as Error).message, { status: 500 });
-  }
-}
+// export async function GET(req: NextRequest, { params }: Params) {
+//   try {
+//     return NextResponse.json({
+//       result: await kv.json.get("products", `$.${params.id}`),
+//     });
+//   } catch (error) {
+//     return new Response((error as Error).message, { status: 500 });
+//   }
+// }
 
-export async function PUT(req: NextRequest, { params }: Params) {
-  try {
-    const product = await req.json();
-    return NextResponse.json({
-      result: await kv.json.set("products", `$.${params.id}`, {
-        id: params.id,
-        ...(product as {}),
-      }),
-      product: product,
-    });
-  } catch (error) {
-    return new Response((error as Error).message, { status: 500 });
-  }
-}
+// export async function PUT(req: NextRequest, { params }: Params) {
+//   try {
+//     const product = await req.json();
+//     return NextResponse.json({
+//       result: await kv.json.set("products", `$.${params.id}`, {
+//         id: params.id,
+//         ...(product as {}),
+//       }),
+//       product: product,
+//     });
+//   } catch (error) {
+//     return new Response((error as Error).message, { status: 500 });
+//   }
+// }
 
-export async function DELETE(req: NextRequest, { params }: Params) {
-  try {
-    return NextResponse.json({
-      result: await kv.json.del("products", `$.${params.id}`),
-    });
-  } catch (error) {
-    return new Response((error as Error).message, { status: 500 });
-  }
-}
+// export async function DELETE(req: NextRequest, { params }: Params) {
+//   try {
+//     return NextResponse.json({
+//       result: await kv.json.del("products", `$.${params.id}`),
+//     });
+//   } catch (error) {
+//     return new Response((error as Error).message, { status: 500 });
+//   }
+// }
